2850aa9a3209a316d0b82721208e104ca6a96830
DESKTOP-NDFI8ST\letam <tamlv.bhsoft@gmail.com>
2022-12-03 00:40:11 +0700

    initial commit

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMaxHealth(int maxHealth) {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+    }
+
+    
+    public void SetMinHealth(int minHealth) {
+        slider.minValue = minHealth;
+        slider.value = minHealth;
+    }
+
+    public void SetHealth(int health) {
+        slider.value = health;
+    }
+}
